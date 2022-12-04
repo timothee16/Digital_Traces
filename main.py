62d9398dc4a3a0958dba6a5f66e7f8b4d037f64a
timothee16 <timothee.rossillon@epfedu.fr>
2022-12-04 18:11:46 +0100

    second commit

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async 
+src="https://www.googletagmanager.com/gtag/js?id=G-PNWR0TFW4H"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'G-PNWR0TFW4H');
+</script>
+ """
+ return prefix_google + "Hello World"
