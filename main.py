f26fe8d0ab02d282392b4f421f42a6f1c26b174f
timothee16 <timothee.rossillon@epfedu.fr>
2022-12-20 22:08:59 +0100

    Last commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 import logging
+import requests
 
 app = Flask(__name__)
 
@@ -20,8 +21,25 @@ src="https://www.googletagmanager.com/gtag/js?id=G-PNWR0TFW4H"></script>
  """
  return prefix_google + "Welcome to our page"
 
-@app.route('/logger', methods=["GET"])
-def logger():
-    script = """
-    <script> console.log("logger") </script>"""
-    return "There is the logger" + script
+@app.route('/logger', methods=['GET'])
+def Message():
+    app.logger.warning('Warning log')
+    app.logger.error('Error log')
+    app.logger.info('Info log')
+
+    return render_template('textbox.html')
+
+@app.route('/getcookie')
+def cookie():
+    req = requests.get("https://www.google.com/")
+    app.logger.info(req)
+    return req.cookies.get_dict()
+
+@app.route('/getcookie2')
+def cookie2():
+    req = requests.get('https://analytics.google.com/analytics/web/#/a250427860p344255484')
+    app.logger.info(req)
+    return req.text
+
+if __name__=="__main__":
+    app.run(debug=True)
